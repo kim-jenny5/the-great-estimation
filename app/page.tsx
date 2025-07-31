51decb5773d05e295ef0f7f6769594de73c65076
Jenny Kim <kimj1905@gmail.com>
2025-07-31 10:01:44 -0400

    add construction banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ConstructionBanner from '@/components/ConstructionBanner';
 import Navbar from '@/components/Navbar';
 import Header from '@/components/Header';
 import Slider from '@/components/Slider';
@@ -8,8 +9,9 @@ import Footer from '@/components/Footer';
 export default function Dashboard() {
   return (
     <>
+      <ConstructionBanner />
       <Navbar />
-      <main className='wrapper min-h-screen flex-col gap-y-6 p-8'>
+      <main className='wrapper min-h-screen flex-col gap-y-6'>
         <Header />
         <StatGroup />
         <Slider />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,12 @@
 export default function Navbar() {
   return (
-    <div className='wrapper items-center justify-between border-b border-black/25'>
+    <nav className='wrapper items-center justify-between border-b border-black/25 pt-12'>
       <div className='flex flex-col text-xl leading-none font-black tracking-tight text-neutral-800'>
         The Great Estimation
       </div>
       <div className='flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-neutral-200 text-sm font-semibold text-neutral-800'>
         JK
       </div>
-    </div>
+    </nav>
   );
 }
