63439b60736835eb576a957d81a5fedb73f98aa1
Jenny Kim <kimj1905@gmail.com>
2025-08-06 14:53:48 -0400

    add notification banner for data reset

--- components/ResetDatabaseBtn.tsx
+++ components/ResetDatabaseBtn.tsx
@@ -1,16 +1,27 @@
 'use client';
 
+import { useState } from 'react';
+import NotificationBanner from './NotificationBanner';
 import { resetDatabase } from '@/util/queries';
 
 export default function ResetDatabaseBtn() {
+	const [showNotification, setShowNotification] = useState(false);
+
+	const handleReset = async () => {
+		await resetDatabase();
+		setShowNotification(true);
+		setTimeout(() => setShowNotification(false), 3300);
+	};
+
 	return (
-		<form action={resetDatabase}>
+		<>
 			<button
-				type='submit'
-				className='cursor-pointer rounded border border-red-600 bg-white px-2 py-1.5 text-xs font-bold text-red-600 uppercase hover:bg-red-600 hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-lime-300'
+				onClick={handleReset}
+				className='cursor-pointer rounded border border-red-600 bg-white px-2 py-1.5 text-xs font-semibold text-red-600 uppercase hover:bg-red-600 hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-lime-300'
 			>
 				Reset Data
 			</button>
-		</form>
+			{showNotification && <NotificationBanner />}
+		</>
 	);
 }
