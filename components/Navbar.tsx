468fda1d62148a7ba5eee4aad5809f688f931feb
Jenny Kim <jennykimdev@gmail.com>
2026-04-21 20:29:38 -0400

    remove redundant fragment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,15 +4,13 @@ import ResetDatabaseBtn from './ResetDatabaseBtn';
 
 export default function Navbar({ user }: { user: string }) {
 	return (
-		<>
-			<nav className='wrapper justify-end border-b border-black/25'>
-				<div className='flex items-center gap-x-4'>
-					<ResetDatabaseBtn />
-					<div className='flex h-10 w-10 items-center justify-center rounded-full bg-neutral-200 text-sm font-semibold text-neutral-800'>
-						{formatInitials(user)}
-					</div>
+		<nav className='wrapper justify-end border-b border-black/25'>
+			<div className='flex items-center gap-x-4'>
+				<ResetDatabaseBtn />
+				<div className='flex h-10 w-10 items-center justify-center rounded-full bg-neutral-200 text-sm font-semibold text-neutral-800'>
+					{formatInitials(user)}
 				</div>
-			</nav>
-		</>
+			</div>
+		</nav>
 	);
 }
